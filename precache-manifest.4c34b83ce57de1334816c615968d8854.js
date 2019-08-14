5318faf057ee44f93a5938c4218b1d27cb2def00
Lypzis <victorvp2008@hotmail.com>
2019-08-14 11:56:41 -0300

    Updates

---
+++ precache-manifest.4c34b83ce57de1334816c615968d8854.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0867a62d565bd62e30464bfcb94f12b6",
+    "url": "/todo_parse_live_query/index.html"
+  },
+  {
+    "revision": "a99557d89e17ff49aa48",
+    "url": "/todo_parse_live_query/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "47b2b3dfa37bcd542840",
+    "url": "/todo_parse_live_query/static/js/2.87833a51.chunk.js"
+  },
+  {
+    "revision": "a99557d89e17ff49aa48",
+    "url": "/todo_parse_live_query/static/js/main.5fd5911d.chunk.js"
+  },
+  {
+    "revision": "ad7f56a915a018ab948c",
+    "url": "/todo_parse_live_query/static/js/runtime~main.1478a615.js"
+  }
+]);
